cf2106b25fd179e41d7506f530d887c158473cbd
Patrik Hildenborg Mac <phildenborg@gmail.com>
2025-04-09 00:48:01 +0200

    script fix

--- script/script.js
+++ script/script.js
@@ -1,13 +1,5 @@
 fetch('txt/date.txt')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Kunde inte läsa datumfilen.');
-        }
-        return response.text();
-    })
+    .then(response => response.text())
     .then(date => {
         document.getElementById("date").innerHTML = date;
-    })
-    .catch(error => {
-        console.error('Fel vid inläsning av datum:', error);
     });
